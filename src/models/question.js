a53b457dceb20f5eeec390155e72c83b23e5f2e5
Mark Anderson <mark.russ.anderson@gmail.com>
2019-07-24 13:53:01 -0400

    Remove all references to the old trivia API. Remove old code.

--- src/models/question.js
+++ src/models/question.js
@@ -13,24 +13,14 @@ const Question = function () {
   this.token;
 };
 
-Question.prototype.bindEvents = function () {    
-    this.getTokenFromAPI();
-
+Question.prototype.bindEvents = function () {
     PubSub.subscribe('Player:question-category', (event) => {
 	const categoryObject = event.detail;
 	const apiCode = categoryObject.apiCode;
 	this.playerID = categoryObject.playerID;
 	this.category = categoryObject.category;
 	
-	//console.log(categoryObject);
-	//console.log(question_ex["0"][0]);
-	
 	var rand_index = Math.floor(Math.random()*question_ex[apiCode].length);
-
-	//console.log(question_ex[apiCode].length);
-	//console.log(rand_index);
-
-	//console.log(question_ex[apiCode][rand_index].Results);
 	
 	this.addQuestionInfo(question_ex[apiCode][rand_index].Results);
 
@@ -38,82 +28,69 @@ Question.prototype.bindEvents = function () {
 	PubSub.publish("Question:question-ready", result);
     });
 
-  PubSub.subscribe('QuestionView:question-answered', (event) => {
-    const chosenAnswer = event.detail;
-    console.log('chosen answer:', chosenAnswer);
-    const result = this.checkAnswer(chosenAnswer);
-    const resultObject = {
-      answer: this.correctAnswer,
-      answerCorrect: result
-    };
-    if (result) {
-      PubSub.publish(`QuestionP${this.playerID}:answer-correct`, this.category);
-    }
-    PubSub.publish('Question:question-result', resultObject);
-  })
+    PubSub.subscribe('QuestionView:question-answered', (event) => {
+	const chosenAnswer = event.detail;
+	console.log('chosen answer:', chosenAnswer);
+	const result = this.checkAnswer(chosenAnswer);
+	const resultObject = {
+	    answer: this.correctAnswer,
+	    answerCorrect: result
+	};
+	if (result) {
+	    PubSub.publish(`QuestionP${this.playerID}:answer-correct`, this.category);
+	}
+	PubSub.publish('Question:question-result', resultObject);
+    })
 };
 
-Question.prototype.getTokenFromAPI = function () {
-  const url = "https://opentdb.com/api_token.php?command=request";
-  const request = new RequestHelper(url);
-  request.get()
-  .then((result) => {this.token = result.token;})
-};
+//Question.prototype.getTokenFromAPI = function () {
+//    const url = "https://opentdb.com/api_token.php?command=request";
+//    const request = new RequestHelper(url);
+//    request.get()
+//	.then((result) => {this.token = result.token;})
+//};
 
 Question.prototype.checkAnswer = function (chosenAnswer) {
-  return chosenAnswer === this.correctAnswer;
+    return chosenAnswer === this.correctAnswer;
 };
 
 Question.prototype.addQuestionInfo = function (apiInfo_arg) {
     var apiInfo = JSON.parse(JSON.stringify(apiInfo_arg));
-
+    
     this.question = apiInfo.question;
     this.correctAnswer = apiInfo['correct_answer'];
-
-    //console.log("before " + apiInfo['correct_answer'])
-    
-    //this.answersArray = ['TEST']
-
     this.answersArray = apiInfo['incorrect_answers'];
-
-    //console.log("before: " + this.answersArray);
-    
     this.answersArray.push(this.correctAnswer);
-
-    //console.log("after " + this.correctAnswer);
-    //console.log("after: " + this.answersArray);
     
     this.answersArray = this.randomiseAnswers(this.answersArray);
 };
 
 Question.prototype.setUpQuestion = function () {
     console.log("setUpQuestion " + this.answersArray);
-
+    
     return {
-    question: this.question,
-      answers: this.answersArray
-
-      
-  };
+	question: this.question,
+	answers: this.answersArray
+    };
 };
 
 
 Question.prototype.randomiseAnswers = function (array) {
-  let currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+	
+	// Pick a remaining element...
+	randomIndex = Math.floor(Math.random() * currentIndex);
+	currentIndex -= 1;
+	
+	// And swap it with the current element.
+	temporaryValue = array[currentIndex];
+	array[currentIndex] = array[randomIndex];
+	array[randomIndex] = temporaryValue;
+    }
+    return array;
 };
 
 module.exports = Question;
