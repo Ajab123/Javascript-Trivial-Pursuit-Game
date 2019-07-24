9e8ec225d8c5e9c67d2807424f18b41c587296bb
opendatastoriesottawa <opendatastoriesottawa@gmail.com>
2019-07-24 12:31:37 -0400

    updated questions.json with new cat list

--- py/json/json_qs.py
+++ py/json/json_qs.py
@@ -5,9 +5,6 @@ import json
 # import csv
 
 
-#define json path here
-jsonpath = "path/to/output"
-
 
 
 """
@@ -18,7 +15,7 @@ headers = ['Response_Code',	'Results']
 results = ['category',	'type',	'difficulty', 'question',	'correct_answer',	'incorrect_answers']
 
 response_code = 0
-category = ["Health", "Tourism", "Geography"]
+category = ["Health", "Tourism", "Geography","Cannabis", "Agriculture", "Nature"]
 resultstype = ['boolean', 'multiple']
 difficulty = 'easy'
 
@@ -30,6 +27,10 @@ questions.append("Please enter your height and weight. Do you think you are abov
 questions.append("ON Region: What is the most populous CMA?")
 questions.append("BC Region: What is the most populous CMA?")
 questions.append("QC Region: What is the most populous CMA?")
+questions.append("Which province had the highest percentage of cannabis users in the first quarter of 2019?")
+questions.append("True or false: More than 60% of food bought by Canadians is produced domestically.")
+questions.append("How much of Canada's Forest Lands are protected?")
+
 
 correct_answer = []
 correct_answer.append("FALSE")
@@ -39,6 +40,9 @@ correct_answer.append('POST-DEFINED')
 correct_answer.append('Toronto')
 correct_answer.append('Vancouver')
 correct_answer.append('Montreal')
+correct_answer.append('Alberta')
+correct_answer.append('TRUE (70%)')
+correct_answer.append('7%')
 
 incorrect_answers = []
 incorrect_answers.append(['TRUE'])
@@ -48,10 +52,11 @@ incorrect_answers.append(["TRUE", "FALSE"])
 incorrect_answers.append(["Ottawa-Gatineau","Windsor","Sudbury"])
 incorrect_answers.append(["Victoria","Kelowna","Abbotsford-Mission"])
 incorrect_answers.append(["Ottawa-Gatineau","Quebec","Sherbrooke"])
+incorrect_answers.append(['Ontario','Quebec','British-Columbia'])
+incorrect_answers.append(['FALSE'])
+incorrect_answers.append(["0%", "50%", "99%"])
 
 
-# build list of dictionaries
-_listdict = []
 
 
 def build_dict(rcode, results, category, restype, diff, q, ans, incorrect):
@@ -72,13 +77,31 @@ def build_dict(rcode, results, category, restype, diff, q, ans, incorrect):
 	
 	return _d
 
-_listdict.append(build_dict(response_code, results, category[0], resultstype[0], difficulty, questions[0], correct_answer[0], incorrect_answers[0]))
-_listdict.append(build_dict(response_code, results, category[1], resultstype[1], difficulty, questions[1], correct_answer[1], incorrect_answers[1]))
-_listdict.append(build_dict(response_code, results, category[0], resultstype[1], difficulty, questions[2], correct_answer[2], incorrect_answers[2]))
-_listdict.append(build_dict(response_code, results, category[0], resultstype[0], difficulty, questions[3], correct_answer[3], incorrect_answers[3]))
-_listdict.append(build_dict(response_code, results, category[2], resultstype[0], difficulty, questions[4], correct_answer[4], incorrect_answers[4]))
-_listdict.append(build_dict(response_code, results, category[2], resultstype[0], difficulty, questions[5], correct_answer[5], incorrect_answers[5]))
-_listdict.append(build_dict(response_code, results, category[2], resultstype[0], difficulty, questions[6], correct_answer[6], incorrect_answers[6]))
+	
+# build lists of dictionaries
+#_listdict = []
+_listdict0 = []
+_listdict1 = []
+_listdict2 = []
+_listdict3 = []
+_listdict4 = []
+_listdict5 = []
+	
+_listdict0.append(build_dict(response_code, results, category[0], resultstype[0], difficulty, questions[0], correct_answer[0], incorrect_answers[0]))
+_listdict0.append(build_dict(response_code, results, category[0], resultstype[1], difficulty, questions[2], correct_answer[2], incorrect_answers[2]))
+_listdict0.append(build_dict(response_code, results, category[0], resultstype[0], difficulty, questions[3], correct_answer[3], incorrect_answers[3]))
+
+_listdict1.append(build_dict(response_code, results, category[1], resultstype[1], difficulty, questions[1], correct_answer[1], incorrect_answers[1]))
+
+_listdict2.append(build_dict(response_code, results, category[2], resultstype[0], difficulty, questions[4], correct_answer[4], incorrect_answers[4]))
+_listdict2.append(build_dict(response_code, results, category[2], resultstype[0], difficulty, questions[5], correct_answer[5], incorrect_answers[5]))
+_listdict2.append(build_dict(response_code, results, category[2], resultstype[0], difficulty, questions[6], correct_answer[6], incorrect_answers[6]))
+_listdict3.append(build_dict(response_code, results, category[3], resultstype[0], difficulty, questions[7], correct_answer[7], incorrect_answers[7]))
+_listdict4.append(build_dict(response_code, results, category[4], resultstype[1], difficulty, questions[8], correct_answer[8], incorrect_answers[8]))
+_listdict5.append(build_dict(response_code, results, category[5], resultstype[0], difficulty, questions[9], correct_answer[9], incorrect_answers[9]))
+
+_listdict = {"0":_listdict0, "1":_listdict1, "2":_listdict2, "3":_listdict3, "4":_listdict4, "5":_listdict5}
+
 
 #print(json.dumps(_listdict))
 
@@ -115,4 +138,4 @@ def write_json(input, path):
 
 	return
 
-write_json(_listdict, jsonpath)	
+write_json(_listdict, r"D:\Dougall\Hackathons\STATCAN\Gamify\questions.json")	
